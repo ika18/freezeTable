4bc470347c64225d803d7e05472236489710de00
ika18 <wunianzu515@gmail.com>
2011-04-29 00:57:43 +0800

    Refined the css for demo page. Refined the js, fix the bug that is break page in Chrome.

--- freezeTable.js
+++ freezeTable.js
@@ -1,3 +1,15 @@
+//
+// Description: 
+// This plugin will hlep you implement a fixed columns table, which looks like freeze columns in excel
+//
+// Note:
+// The plugin will reset your table's CELLSPACING and CELLPADDING to zero
+//
+// Author: Ika Wu
+// Email: wunianzu515@gmail.com
+//
+//
+
 (function( $ ){
 
   $.fn.freezeTable = function( options ) {  
@@ -16,39 +28,72 @@
 
 		// plugin code here
 
-		var $table = $(this);
+		var $this = $(this);
 
-		var $container = $("<div class='ztable'><div class='fixedTable'></div><div class='scrollTable'></div></div>");
-		// insert container
-		$container.insertAfter($("table.ztable"));
-
-		var $current_root = $table.parents().find('div.ztable');
-		// append original table to div.scrollTable
-		$("div.scrollTable").append($table);
-
-		var $scrollTable = $current_root.find("div.scrollTable table");
-		// append copy of oringil table to div.fixedTable
-		$("div.ztable div.fixedTable").append($scrollTable.clone());
-
-		var fixedColumn = settings.fixedColumn;
-		var scrollWidth = settings.scrollWidth;
-
-		var fixedWidth = 0;
-		for (var i = 1; i <= fixedColumn; i++) {
-			fixedWidth = fixedWidth + $current_root.find("div.fixedTable thead th:nth-child(" + i + ")").outerWidth(true);
+		if ($this.attr("tagName") != "TABLE") {
+			alert("This is not a TABLE element!");
+			return false;
 		}
 
-		// set specific width to div.fixedTable, then it looks like have frozen columns
-		$current_root.find("div.fixedTable").width(fixedWidth);
-		// hide frozen columns in scroll table
-		$current_root.find("div.scrollTable table").width(fixedWidth).css({
-			position:"relative",
-			left: - fixedWidth
+		$this.attr({
+			'cellspacing': 0,
+			'cellpadding': 0
 		});
-		// set scroll area width
-		$current_root.find("div.scrollTable").css({
-			width:scrollWidth
-		});
+
+		var fColumns = settings.fixedColumn;
+		var sWidth = settings.scrollWidth;
+
+		var tColumns = $this.find("td").length;
+
+		var cWidth = 0;
+
+		var container = "<div class='fa'><div class='ha'></div><div class='ra'></div><div style='clear:both;'></div><div class='ca'></div><div class='sa'></div><div style='clear:both'></div></div>";
+		$(container).insertBefore($this);
+
+		$this.prev("div.fa").find("div.sa").append($this);
+
+		$root = $this.parents("div.fa");
+		$sa = $root.find("div.sa");
+		$ca = $root.find("div.ca");
+
+		if (fColumns < 0){
+			alert("Columns cann't be smaller than 0!");
+			return false;
+		} else if (fColumns == 0) {
+			$sa.css({
+				'overflow-x': 'scroll',
+				width: sWidth
+			});
+		} else if (fColumns > 0 && fColumns < tColumns) {
+			$cTable = $this.clone();
+			$ca.append($cTable);
+
+			for (var i = 1; i <= fColumns; i++) {
+				cWidth += $ca.find("table tr td:nth-child(" + i + ")").outerWidth();
+			}
+
+			console.log(cWidth);
+
+			$ca.css({
+				'overflow': 'hidden',
+				width: cWidth
+			});
+
+			$sa.css({
+				'overflow-x': 'scroll',
+				width: sWidth
+			});
+			$this.css({
+				position: 'relative',
+				left: - cWidth
+			});
+
+		} else {
+			alert("Are you sure your table having such columns?");
+			return false;
+		}
+
+		
 
 	});
 
